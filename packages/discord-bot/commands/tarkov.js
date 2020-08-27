28fbf8361c95182d11b771b6b2f0357c2e541fa6
Marc David <zubben@gmail.com>
2020-08-27 19:25:15 +0100

    Tarkov commands (only S Tier Loot for now)

---
+++ packages/discord-bot/commands/tarkov.js
@@ -0,0 +1,39 @@
+'use strict';
+const Discord = require('discord.js');
+const puppeteer = require('puppeteer');
+module.exports = {
+	name: 'tarkov',
+	description: 'top loot',
+	execute(message) {
+		(async function main() {
+			try {
+				const browser = await puppeteer.launch();
+				const [page] = await browser.pages();
+
+				await page.goto('https://tarkov-tools.com/loot-tier/barter-items');
+
+				const lootItems = await page.evaluate(
+					() => Array.from(document.querySelectorAll('img[alt]'),
+						a => a.getAttribute('alt'),
+					),
+				);
+				const price = await page.evaluate(
+					() => document.querySelector('div[class="price-range-wrapper"]').innerText,
+
+				);
+
+				const lootInfo = new Discord.MessageEmbed()
+					.setDescription('**S TIER LOOT**')
+					.addField(`${price.split('\n').splice(0, 1)}`, lootItems.slice(0, 27));
+
+				console.log(price);
+
+				await browser.close();
+				return message.channel.send(lootInfo);
+			}
+			catch (err) {
+				console.error(err);
+			}
+		})();
+	},
+};
