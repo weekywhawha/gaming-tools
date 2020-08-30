cbefd9994cb901f33772005046c391ce39fe18c0
Marc David <zubben@gmail.com>
2020-08-30 17:53:25 +0100

    file system search tool

---
+++ packages/discord-bot/libs/utils/file-system.js
@@ -0,0 +1,30 @@
+import { readdir } from "fs/promises";
+import { join, basename } from "path";
+
+async function* dirs(path = ".") {
+  yield path;
+  for (const dirent of await readdir(path, { withFileTypes: true }))
+    if (dirent.isDirectory()) yield* dirs(join(path, dirent.name));
+    else yield join(path, dirent.name);
+}
+
+async function* empty() {
+  // empty
+}
+
+async function toArray(iter = empty()) {
+  const r = [];
+  for await (const x of iter) r.push(x);
+  return r;
+}
+
+// eslint-disable-next-line no-unused-vars
+async function search(iter = empty(), test = (_) => false) {
+  for await (const p of iter) if (test(p)) return p;
+}
+
+async function searchByName(iter = empty(), query = "") {
+  return search(iter, (p) => basename(p) === query);
+}
+
+export { dirs, toArray, search, searchByName };
