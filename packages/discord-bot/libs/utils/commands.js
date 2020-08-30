f086b2f5c2d4c9136469974a3fb0e4f8ccf8eac5
Marc David <zubben@gmail.com>
2020-08-30 18:00:18 +0100

    commands now only deals with commands

--- packages/discord-bot/libs/utils/commands.js
+++ packages/discord-bot/libs/utils/commands.js
@@ -1,9 +1,8 @@
-import { readdir, stat } from "fs/promises";
 import Discord from "discord.js";
 import path from "path";
+import { dirs, toArray } from "./file-system.js";
 const __dirname = path.resolve();
 
-const COMMANDS_PATH = path.resolve(__dirname, "commands");
 const commands = new Discord.Collection();
 
 export const getCommands = async function () {
@@ -11,36 +10,14 @@ export const getCommands = async function () {
     return commands;
   }
 
-  // Reads normal .js files in the main dir
-  const names = await readdir(COMMANDS_PATH);
-  const commandFiles = names.filter((file) => file.endsWith(".js"));
-  // Loops through all the folders in the main dir and finds those with a .js extension
-  for (const folder of commandFiles) {
-    const stats = await stat(path.resolve(COMMANDS_PATH, folder));
-    if (stats.isDirectory()) {
-      const namesInFolder = await readdir(path.resolve(COMMANDS_PATH, folder));
-      const folderFiles = namesInFolder.filter((file) => file.endsWith(".js"));
-      for (const file of folderFiles) {
-        // @ts-ignore
-        commandFiles.push([folder, file]);
-      }
-    }
-  }
+  const files = await toArray(dirs("./commands"));
+  const commandFiles = files.filter((file) => file.endsWith(".js"));
 
-  // Takes the two different command and folder lists and requires all the commands into an array which then puts it into the collection
   for (const file of commandFiles) {
-    let command;
-    if (Array.isArray(file)) {
-      command = await import(
-        `file:///${path.resolve(COMMANDS_PATH, file[0], file[1])}`
-      ).then((m) => m.default);
-    } else {
-      command = await import(
-        `file:///${path.resolve(COMMANDS_PATH, file)}`
-      ).then((m) => m.default);
-    }
+    const command = await import(
+      `file:///${path.resolve(__dirname, file)}`
+    ).then((m) => m.default);
     commands.set(command.name, command);
   }
-
   return commands;
 };
