c7508a4ad80cf3b523cc36751a06ea620abbe8a7
Marc David <zubben@gmail.com>
2020-09-01 20:27:19 +0100

    Merge branch 'develop' into master

--- packages/discord-bot/commands/moderation/clear.js
+++ packages/discord-bot/commands/moderation/clear.js
@@ -4,6 +4,8 @@ export default {
   usage: "[number]",
   execute(message, args) {
     const amount = parseInt(args[0]) + 1;
+    if (!message.member.roles.cache.some((r) => r.name === "Admin"))
+      return message.reply("you do not have permission to use this command");
 
     if (isNaN(amount)) {
       return message.reply("that doesn't seem to be a valid number.");
--- packages/discord-bot/commands/moderation/kick.js
+++ packages/discord-bot/commands/moderation/kick.js
@@ -2,13 +2,31 @@ export default {
   name: "kick",
   description: "Tag a member and kick them (but not really).",
   guildOnly: true,
-  execute(message) {
-    if (!message.mentions.users.size) {
-      return message.reply("you need to tag a user in order to kick them!");
-    }
+  async execute(message, args) {
+    if (!message.member.roles.cache.some((r) => r.name === "Admin"))
+      return message.reply("you do not have permission to use this command");
 
-    const taggedUser = message.mentions.users.first();
+    const member =
+      message.mentions.members.first() || message.guild.members.get(args[0]);
+    if (!member)
+      return message.reply("Please mention a valid member of this server");
+    if (!member.kickable)
+      return message.reply(
+        "I cannot kick this user! Do they have a higher role? Do I have kick permissions?"
+      );
 
-    message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    let reason = args.slice(1).join(" ");
+    if (!reason) reason = "No reason provided";
+
+    await member
+      .kick(reason)
+      .catch((error) =>
+        message.reply(
+          `Sorry ${message.author} I couldn't kick because of : ${error}`
+        )
+      );
+    message.channel.send(
+      `<@${member.user.id}> has been kicked by <@${message.author.id}>. **Reason:** ${reason}`
+    );
   },
 };
