c51fc50c6b91272d229b2521ac091e72dc9dbe49
Marc David <zubben@gmail.com>
2020-09-03 01:49:21 +0100

    prettier

--- packages/discord-bot/commands/games/dual.js
+++ packages/discord-bot/commands/games/dual.js
@@ -1,37 +1,35 @@
-import pkg from 'googleapis';
+import pkg from 'googleapis'
 const { google } = pkg
 
 const spreadsheetId = '14iHVub5lhpK4_IeshhHALnRyKDbob14npwo7OPCFvwM'
 const APIKey = process.env.GOOGLE_API_KEY
-const sheets = google.sheets({version: "v4", auth: APIKey});
+const sheets = google.sheets({ version: 'v4', auth: APIKey })
 
 export default {
   name: 'dual',
-  description:'dual universe',
+  description: 'dual universe',
   usage: '[argument]',
   async execute(message) {
-    
-    if(message)
-      message.reply('getting the information')
+    if (message) message.reply('getting the information')
 
     sheets.spreadsheets.get({ spreadsheetId }, (err, res) => {
       if (err) {
-        console.error(err);
-        return;
+        console.error(err)
+        return
       }
       sheets.spreadsheets.values.batchGet(
         {
           spreadsheetId,
-          ranges: res.data.sheets.map(e => e.properties.title)
+          ranges: res.data.sheets.map((e) => e.properties.title),
         },
         (err, res) => {
           if (err) {
-            console.error(err);
-            return;
+            console.error(err)
+            return
           }
-          console.log(JSON.stringify(res.data));
+          console.log(JSON.stringify(res.data))
         }
-      );
-    });
-  }
+      )
+    })
+  },
 }
