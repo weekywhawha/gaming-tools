8a1f4d45c348c2385ae8537273f926e8c287f803
Marc David <zubben@gmail.com>
2020-09-07 17:24:46 +0100

    this command now accepts comments as an argument

--- packages/discord-bot/commands/misc/roll.js
+++ packages/discord-bot/commands/misc/roll.js
@@ -5,14 +5,23 @@ export default {
   name: 'roll',
   description:
     'Executes a command given in dice notation, and returns the results.\nSee https://greenimp.github.io/rpg-dice-roller/guide/notation/ for more information about dice notation.',
-  usage: '[dice-notation]',
+  usage: '[dice-notation] / [comment]',
   execute(message, args) {
-    if (!args[0]) return message.reply('Please specify a dice type e.g., "1d6", "2d5 + 1d7", etc.')
+    if (!args[0]) return message.reply('Please specify a dice type e.g., "1d6", "2d5+1d7", etc.')
 
     try {
-      dice.roll(args.join(''))
+      dice.roll(args[0])
 
       const result = dice.log.shift()
+
+      if (args[1] === '/') {
+        
+        const comment = args.splice(2).join(" ")
+
+        return message.channel.send(
+          `${message.author} rolled**${result.toString().substring(result.toString().indexOf(':') + 1)}** | *${comment}*`
+        )
+      }
 
       return message.channel.send(
         `${message.author} rolled**${result.toString().substring(result.toString().indexOf(':') + 1)}**`
