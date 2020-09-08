7ec99df62e4546857ab1daa99faa0722d0936ba9
Marc David <zubben@gmail.com>
2020-09-08 21:10:59 +0100

    dual command to access google spreadhseet

--- packages/discord-bot/commands/games/dual.js
+++ packages/discord-bot/commands/games/dual.js
@@ -1,7 +1,7 @@
 import pkg from 'googleapis'
+import { MessageEmbed } from 'discord.js'
 const { google } = pkg
 
-const spreadsheetId = '14iHVub5lhpK4_IeshhHALnRyKDbob14npwo7OPCFvwM'
 const APIKey = process.env.GOOGLE_API_KEY
 const sheets = google.sheets({ version: 'v4', auth: APIKey })
 
@@ -9,27 +9,39 @@ export default {
   name: 'dual',
   description: 'dual universe',
   usage: '[argument]',
-  async execute(message) {
-    if (message) message.reply('getting the information')
+  async execute(message, args) {
+    const searchInput = args[0]
+    if (!searchInput || searchInput.length < 4) return message.reply(`invalid search parameter`)
+    const request = {
+      spreadsheetId: '14iHVub5lhpK4_IeshhHALnRyKDbob14npwo7OPCFvwM',
+      range: 'A2:V37',
+      majorDimension: 'COLUMNS',
+    }
 
-    sheets.spreadsheets.get({ spreadsheetId }, (err, res) => {
-      if (err) {
-        console.error(err)
-        return
-      }
-      sheets.spreadsheets.values.batchGet(
-        {
-          spreadsheetId,
-          ranges: res.data.sheets.map((e) => e.properties.title),
-        },
-        (err, res) => {
-          if (err) {
-            console.error(err)
-            return
-          }
-          console.log(JSON.stringify(res.data))
-        }
+    const sheet = await sheets.spreadsheets.values.get(request)
+
+    const findElement = sheet.data.values.find((element) =>
+      element.toString().toLocaleLowerCase().includes(searchInput)
+    )
+
+    if (!findElement) return message.reply('your search did not match any item.')
+
+    const oreName = findElement[0]
+    const oreValues = findElement.slice(7)
+    const planets = sheet.data.values[0].slice(7)
+
+    const oreInfo = new MessageEmbed()
+      .setTitle(`${oreName}`)
+      .addField(
+        'Items',
+        planets.map((str) => str.substring()),
+        true
       )
-    })
+      .addField('\u200b', '\u200b', true)
+      .addField('Depth', `${oreValues.toString().replace(/,/g, ' \u200b\n')}`, true)
+      .addField('\u200b', '\u200b', false)
+      .setFooter('source: docs.google.com', 'https://ssl.gstatic.com/docs/common/product/sheets_app_icon1.png')
+
+    await message.channel.send(oreInfo)
   },
 }
