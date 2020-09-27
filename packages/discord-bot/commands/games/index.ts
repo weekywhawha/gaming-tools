274e563006d00c5a74a06bf6082c46009611b864
Marc David <zubben@gmail.com>
2020-09-27 15:49:15 +0100

    fix(core): enabled commands again

--- packages/discord-bot/commands/games/index.ts
+++ packages/discord-bot/commands/games/index.ts
@@ -1,13 +1,13 @@
 import { dual } from './dual'
-//import { tarkov } from './tarkov'
+import { tarkov } from './tarkov'
 import { twitch } from './twitch'
 
 export * from './dual'
-//export * from './tarkov'
+export * from './tarkov'
 export * from './twitch'
 
 export const gamesCommands = {
   dual,
-  //tarkov,
+  tarkov,
   twitch,
 }
