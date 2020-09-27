8e9f036a931b3b5a3e68559686be3505c2e2eae7
Marc David <zubben@gmail.com>
2020-09-27 15:48:31 +0100

    fix(core): weather interface

---
+++ packages/types/weather-info.ts
@@ -0,0 +1,12 @@
+export interface WeatherInfo {
+  skytext: string
+  observationpoint: string
+  imageUrl: string
+  date: string
+  winddisplay: string
+  temperature: number
+  feelslike: number
+  humidity: number
+  timezone: number
+}
+
