8f5d5b625bc8803a8957665096b4d3be0d6dab49
weekywhawha <zubben@gmail.com>
2020-10-02 17:11:56 +0100

    feature(api): new weather api controller

---
+++ packages/api/controller/weather.ts
@@ -0,0 +1,16 @@
+import { ApiError } from '../error/api-error'
+import { getWeatherInfo } from '@gaming-tools/libraries'
+import { Info } from '../types/info'
+
+export class Weather {
+  static  info: Info =  async (req, res, next) => {
+    
+    const { location } = req.body
+
+    try {
+      res.send(await getWeatherInfo(location))
+    } catch (error) {
+      next(ApiError.badRequest(error))
+    }
+  }
+}
