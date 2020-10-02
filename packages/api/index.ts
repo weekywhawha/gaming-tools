cf9dc1e274e3297fe8bfc88574f57fbd229e85c9
weekywhawha <zubben@gmail.com>
2020-10-02 17:10:14 +0100

    refactor(api): cleaning code

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,35 +1,15 @@
-import express from 'express'
 require('dotenv').config()
+import express from 'express'
 import { router } from './routes/index'
-import {errorHandler} from './error/error-handler'
-//import { getTwitchInfo } from '@gaming-tools/libraries'
+import { errorHandler } from './error/api-error-handler'
 
 const port = process.env.PORT
 
-
-
 const app = express()
 
+app.use(express.json())
 app.use('/', router)
 
-
-
-
-
 app.use(errorHandler)
-
-
-
-
-
-// app.get('/api/twitch/:id', async (req, res) => {
-//     res.send(await getTwitchInfo(req.params.id))
-//     next(err)
-// })
-
-// app.use(function (err, req, res, next) {
-//   console.error(err.stack)
-//   res.status(500).send('Something went wrong.')
-// })
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
