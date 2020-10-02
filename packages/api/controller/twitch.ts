81c4aa66e7eeb87e0460e3d9340716a5b209f89a
weekywhawha <zubben@gmail.com>
2020-10-02 19:38:51 +0100

    refactor(api): prettier

--- packages/api/controller/twitch.ts
+++ packages/api/controller/twitch.ts
@@ -3,8 +3,7 @@ import { getTwitchInfo } from '@gaming-tools/libraries'
 import { Info } from 'api/types/info'
 
 export class Twitch {
-  static  info: Info =  async (req, res, next) => {
-    
+  static info: Info = async (req, res, next) => {
     const { id } = req.body
 
     try {
