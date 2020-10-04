316138680d3e76f3eb84fc6e7961e5a3d001d566
weekywhawha <zubben@gmail.com>
2020-10-05 00:53:31 +0100

    fix(discord-bot): attachment

--- packages/discord-bot/commands/games/twitch.ts
+++ packages/discord-bot/commands/games/twitch.ts
@@ -16,7 +16,8 @@ export const twitch: Command = {
         .addField('Title', `${twitchInfo.title}`)
         .addField('Game', `${twitchInfo.gameName}`, true)
         .addField('Viewers', `${twitchInfo.viewers} viewers`, true)
-        .setImage(`${twitchInfo.thumbnail}`)
+        .attachFiles([twitchInfo.thumbnail])
+        .setImage(`attachment://${twitchInfo.thumbnail.replace('https://static-cdn.jtvnw.net/previews-ttv/', '')}`)
         .setURL(`https://www.twitch.tv/${twitchInfo.name}`)
         .setFooter('source: twitch.tv', 'https://static.twitchcdn.net/assets/favicon-32-d6025c14e900565d6177.png')
 
